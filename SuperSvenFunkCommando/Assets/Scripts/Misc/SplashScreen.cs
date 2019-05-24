dcc20e6685842f1c038598eab2a2e299a610824c
Gordon Connell <gordonlconnell@gmail.com>
2019-05-24 14:23:16 +1000

    Sven Model

--- SuperSvenFunkCommando/Assets/Scripts/Misc/SplashScreen.cs
+++ SuperSvenFunkCommando/Assets/Scripts/Misc/SplashScreen.cs
@@ -1,11 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SplashScreen : MonoBehaviour {
 
-	// Use this for initialization
+    Image uiImage;
+    Canvas parentCanvas;
+
+    [SerializeField]
+    Sprite[] images;
+
+    [SerializeField]
+    float fadeTime;
+
+    [SerializeField]
+    float displayTime;
+
+    [SerializeField]
+    float transparentTime;
+
 	void Start () {
+
+        parentCanvas = GetComponent<Canvas>();
+
+        if (parentCanvas.worldCamera != Camera.main)
+            parentCanvas.worldCamera = Camera.main;
+
+        DontDestroyOnLoad(gameObject);
+
+        uiImage = GetComponentInChildren<Image>();
+
+        uiImage.sprite = images[0];
+
+        //StartCoroutine(CycleImages());
 		
 	}
 	
@@ -13,4 +42,16 @@ public class SplashScreen : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    /*IEnumerator CycleImages()
+    {
+        if (!clickToProceed)
+        {
+            for(int i = 0; i < images.Length; i++)
+            {
+                uiImage.sprite = images[i];
+                uiImage;
+            }
+        }
+    }*/
 }
---
+++ SuperSvenFunkCommando/Assets/Scripts/Tools/Punch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    Animator m_Animator;
+    public bool canLeftP;
+    public bool canRightP;
+
+	// Use this for initialization
+	void Start () {
+        canLeftP = true;
+        canRightP = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if ((canLeftP == true) && (canRightP == true))
+            {
+                m_Animator.SetBool("LeftPunch", true);
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if ((canLeftP == true) && (canRightP == true))
+            {
+                m_Animator.SetBool("RightPunch", true);
+            }
+        }
+
+    }
+
+}
+
