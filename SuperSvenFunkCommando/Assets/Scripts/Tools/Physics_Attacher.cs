03654f90a30be0cdb4df51e0c7d890afe8838094
Hoson <hoson_zhong@hotmail.com>
2019-05-24 11:52:00 +1000

    Created breakable wall and basic player prefabs

--- SuperSvenFunkCommando/Assets/Scripts/Tools/Physics_Attacher.cs
+++ SuperSvenFunkCommando/Assets/Scripts/Tools/Physics_Attacher.cs
@@ -16,7 +16,8 @@ public class Physics_Attacher : MonoBehaviour
             mr.gameObject.AddComponent<Rigidbody>();
             MeshCollider collider = mr.gameObject.AddComponent<MeshCollider>();
             collider.convex = true;
-            mr.tag = "Shard";
+            if (mr.name.Contains("shard"))
+                mr.tag = "Shard";
         }
 	}
 }
