0e84cd9d9f35c2ccdd88e7ffccbfebf289902774
Unknown <hoson_zhong@hotmail.com>
2019-05-27 17:52:34 +1000

    Updated to latest splashscreen

--- SuperSvenFunkCommando/Assets/Scripts/Misc/SplashScreen.cs
+++ SuperSvenFunkCommando/Assets/Scripts/Misc/SplashScreen.cs
@@ -15,11 +15,9 @@ public class SplashScreen : MonoBehaviour
         public float lerp;
     }
 
-    public GameObject startButton;
-    public GameObject quitButton;
-    public GameObject blackPanel;
-    public GameObject titleImage;
-    public States[] states_array;
+    public GameObject mainMenu;
+
+    //public States[] states_array;
 
     private VideoPlayer video_player;
     private Image splash_screen;
@@ -31,10 +29,7 @@ public class SplashScreen : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        startButton.SetActive(false);
-        quitButton.SetActive(false);
-        blackPanel.SetActive(false);
-        titleImage.SetActive(false);
+        mainMenu.SetActive(false);
 
         video_player = gameObject.GetComponent<VideoPlayer>();
         video_player.loopPointReached += VideoEnded;
@@ -48,86 +43,85 @@ public class SplashScreen : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (is_running)
-            SplashTransition();
+        //if (is_running)
+        //    SplashTransition();
 	}
 
-    void SplashTransition()
-    {
-        if (state_index < states_array.Length)
-        {
-            Color solid_colour = states_array[state_index].colour;
-            Color faded_colour = solid_colour;
-            faded_colour.a = 0;
+    //void SplashTransition()
+    //{
+    //    if (state_index < states_array.Length)
+    //    {
+    //        Color solid_colour = states_array[state_index].colour;
+    //        Color faded_colour = solid_colour;
+    //        faded_colour.a = 0;
 
-            splash_screen.sprite = states_array[state_index].image;
+    //        splash_screen.sprite = states_array[state_index].image;
 
-            if (phase_index < 3)
-            {
-                state_timer += Time.deltaTime;
+    //        if (phase_index < 3)
+    //        {
+    //            state_timer += Time.deltaTime;
 
-                switch (phase_index)
-                {
-                    case 0:
-                        {
-                            float t = state_timer / states_array[state_index].lerp;
-                            splash_screen.color = Color.Lerp(faded_colour, solid_colour, t);
+    //            switch (phase_index)
+    //            {
+    //                case 0:
+    //                    {
+    //                        float t = state_timer / states_array[state_index].lerp;
+    //                        splash_screen.color = Color.Lerp(faded_colour, solid_colour, t);
 
-                            if (state_timer > states_array[state_index].lerp)
-                            {
-                                state_timer -= states_array[state_index].lerp;
-                                ++phase_index;
-                            }
+    //                        if (state_timer > states_array[state_index].lerp)
+    //                        {
+    //                            state_timer -= states_array[state_index].lerp;
+    //                            ++phase_index;
+    //                        }
 
-                            break;
-                        }
-                    case 1:
-                        {
-                            if (state_timer > states_array[state_index].duration)
-                            {
-                                state_timer -= states_array[state_index].duration;
-                                ++phase_index;
-                            }
+    //                        break;
+    //                    }
+    //                case 1:
+    //                    {
+    //                        if (state_timer > states_array[state_index].duration)
+    //                        {
+    //                            state_timer -= states_array[state_index].duration;
+    //                            ++phase_index;
+    //                        }
 
-                            break;
-                        }
-                    case 2:
-                        {
-                            float t = state_timer / states_array[state_index].lerp;
-                            splash_screen.color = Color.Lerp(solid_colour, faded_colour, t);
+    //                        break;
+    //                    }
+    //                case 2:
+    //                    {
+    //                        float t = state_timer / states_array[state_index].lerp;
+    //                        splash_screen.color = Color.Lerp(solid_colour, faded_colour, t);
 
-                            if (state_timer > states_array[state_index].lerp)
-                            {
-                                state_timer -= states_array[state_index].lerp;
-                                phase_index = 0;
-                                ++state_index;
-                            }
+    //                        if (state_timer > states_array[state_index].lerp)
+    //                        {
+    //                            state_timer -= states_array[state_index].lerp;
+    //                            phase_index = 0;
+    //                            ++state_index;
+    //                        }
 
-                            break;
-                        }
-                    default:
-                        {
-                            Debug.Log("Splash Screen phase switch statement out of index");
-                            break;
-                        }
-                }
-            }
-        }
-        else
-        {
-            startButton.SetActive(true);
-            quitButton.SetActive(true);
-            titleImage.SetActive(true);
+    //                        break;
+    //                    }
+    //                default:
+    //                    {
+    //                        Debug.Log("Splash Screen phase switch statement out of index");
+    //                        break;
+    //                    }
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        startButton.SetActive(true);
+    //        quitButton.SetActive(true);
 
-            is_running = false;
-        }
-    }
+    //        is_running = false;
+    //    }
+    //}
 
     void VideoEnded(UnityEngine.Video.VideoPlayer vp)
     {
         vp.Stop();
         is_running = true;
 
-        blackPanel.SetActive(true);
+        mainMenu.SetActive(true);
     }
 }
